80b82fca3237526002862813743773adaeed4920
James Laverne <james@salad.moe>
2024-06-18 23:13:11 +1000

    Add Addresser type interface

---
+++ utils.go
@@ -0,0 +1,13 @@
+package migagoapi
+
+type Addresser interface {
+	GetAddress() string
+}
+
+func (m Mailbox) GetAddress() string {
+	return m.Address
+}
+
+func (i Identity) GetAddress() string {
+	return i.Address
+}

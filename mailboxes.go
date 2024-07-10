53ef98bc352add3f7dd3f2aaa7eb18dce6ef4fb5
James Laverne <james@salad.moe>
2024-07-10 22:17:53 +1000

    Potential fix for time parsing issues

--- mailboxes.go
+++ mailboxes.go
@@ -12,7 +12,7 @@ const subpath = "mailboxes"
 type CustomTime time.Time
 
 func (t *CustomTime) UnmarshalJSON(b []byte) (err error) {
-	date, err := time.Parse(`"2006-01-02T15:04:05.000-0700"`, string(b))
+	date, err := time.Parse(`"`+time.RFC3339+`"`, string(b))
 	if err != nil {
 		return err
 	}
